2cb40c20836728075cc04d030eeac84e31952a37
jvn <jvn@mikiaholding.dk>
2011-11-15 18:06:52 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onskeliste::Application.config.secret_token = 'f9eadebb274064a512d5c5af4ec65145a93854ece821ce3f781653028c6e086826c64b4f7bc74c755694acaa366357fe28b770cf22e6d8e3b2000ae9efad1388'
